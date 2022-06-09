08ec5ac3a6a29354d755b3a451cbaa126d3182f4
samuelffn <samuelffn@gmail.com>
2022-06-09 19:00:35 -0300

    Desfazendo as coisas

--- packages/clients/express/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const routes = express.Router();
-
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
-});
-
-export default routes;
